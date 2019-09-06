b29b74eccc6e4a9d71b7f4f74c08e6a393460bf8
Matejson <mateusz.zaborowskii@gmail.com>
2019-09-06 22:43:26 +0200

    Initial commit

---
+++ parallax.js
@@ -0,0 +1,16 @@
+$(document).on("scroll", function () {
+
+    var pixels = $(document).scrollTop()
+
+    $('#sun').css('top', -(pixels ** -0.1) + '%');
+    $('#clouds0').css('top', -(pixels * -0.05) + '%');
+    $('#clouds1').css('top', -(pixels * 0.175) + '%');
+    $('#clouds2').css('top', -(pixels * 0.15) + '%');
+    $('#overlay').css('top', -(pixels * 0.1) +'%');
+    $('#clouds3').css('top', -(pixels * 0.1) + '%');
+    $('#birds').css('top', -(pixels * 0) + '%');
+})
+
+
+
+
